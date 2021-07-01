a18c4004577447a734e33d335e6653b55745ef1f
Jean <jean.bourgarel@epitech.eu>
2021-07-01 16:55:32 +0200

    setting up data view binding

--- app/src/main/java/com/weather/home/HomeFragment.kt
+++ app/src/main/java/com/weather/home/HomeFragment.kt
@@ -6,13 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.weather.R
+import com.weather.databinding.FragmentHomeBinding
 import io.uniflow.android.AndroidDataFlow
+
+import org.koin.android.ext.android.inject
+
 
 class HomeViewModel: AndroidDataFlow() {
 
 }
 
-class HomeFragment : Fragment() {
+class HomeFragment: Fragment() {
+
+    val HomeViewModel: HomeViewModel by inject()
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
