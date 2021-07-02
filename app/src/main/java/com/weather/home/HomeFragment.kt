5728119830141246759f4dad0871c22530370afa
Jean <jean.bourgarel@epitech.eu>
2021-07-02 19:00:27 +0200

    added first events and states

--- app/src/main/java/com/weather/home/HomeFragment.kt
+++ app/src/main/java/com/weather/home/HomeFragment.kt
@@ -5,15 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.weather.R
+import com.weather.api.WeatherApiJSON
 import com.weather.databinding.FragmentHomeBinding
 import io.uniflow.android.AndroidDataFlow
-
+import io.uniflow.core.flow.data.UIEvent
+import io.uniflow.core.flow.data.UIState
 import org.koin.android.ext.android.inject
 
+sealed class HomeState : UIState()
+object FetchingData: HomeState()
+object Idle: HomeState()
+data class Result(val data: String) : HomeState()
+
+sealed class HomeEvent : UIEvent() {
+    data class ClickOnItem(val data: WeatherApiJSON) : HomeEvent()
+//    data class ClickOnItem(val holder: PlayerCardHolder) : HomeEvent()
+}
 
 class HomeViewModel: AndroidDataFlow() {
+    init {
+        action {
+            setState(Idle)
+        }
+    }
 
+    fun fetchingData() {
+        action {
+            setState(FetchingData)
+        }
+    }
+
+    fun fetchingFinished() {
+        action {
+            setState(Idle)
+        }
+    }
+
+    fun clickOnItem(data: WeatherApiJSON) {
+        action {
+            sendEvent(HomeEvent.ClickOnItem(data))
+        }
+    }
 }
 
 class HomeFragment: Fragment() {
@@ -27,5 +62,15 @@ class HomeFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tv1.setOnClickListener {
+            Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
+        }
+/*        binding.newGame.onClick {
+            findNavController().navigate(HomeFragmentDirections.launchSelectPlayersFragment())
+        }*/
     }
 }
